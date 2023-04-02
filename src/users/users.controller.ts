c7d8afee038b81609b6ad8974710cd4464ab695f
kjyong702 <kjyong702@naver.com>
2023-04-02 09:17:20 +0900

    heroku 수정2

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,17 @@
-import { Controller, Delete, Get, Param, Query } from '@nestjs/common';
+import { Controller, Delete, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { AuthService } from './auth.service';
+// import { AuthService } from './auth.service';
 
 @Controller('users')
 export class UsersController {
   constructor(
-    private usersService: UsersService,
-    private authService: AuthService,
+    private usersService: UsersService, // private authService: AuthService,
   ) {}
 
-  @Get('/kakao/callback')
-  async kakaoLogin(@Query('code') code: string) {
-    return this.authService.kakaoLogin(code);
-  }
+  // @Get('/kakao/callback')
+  // async kakaoLogin(@Query('code') code: string) {
+  //   return this.authService.kakaoLogin(code);
+  // }
 
   @Delete('/:id')
   async deleteUser(@Param('id') id: string) {
